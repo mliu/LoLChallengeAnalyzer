e19f792bdce6e4b2f12b87137d9209c223951b10
Michael Liu <michael.liu95@gmail.com>
2015-04-04 16:22:48 -0500

    Sqlite3

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var config = require('./config.js');
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('mydb.db');
 var lolApi = require('leagueapi');
 
 // Initialize League API Poller
@@ -9,8 +11,7 @@ function storeMatchData(matchId) {
 	lolApi.getMatch(matchId, true, 'na', function(err, response) {
 		if(!err) {
 			for ( participant in response.participants ) {
-				var data = 
-				{
+				var data = {
 					"kills": response.participants[participant].stats.kills,
 					"assists": response.participants[participant].stats.assists,
 					"deaths": response.participants[participant].stats.deaths,
@@ -19,6 +20,10 @@ function storeMatchData(matchId) {
 					"cs": response.participants[participant].stats.minionsKilled 
 				}
 				console.log(data);
+				db.serialize(function() {
+					db.run("CREATE TABLE if not exists " + response.participants[participant].championId + " (kills INTEGER) (assists INTEGER) (deaths INTEGER) (win BOOLEAN) (gold INTEGER) (cs INTEGER)");
+					db.run("INSERT INTO " + response.participants[participant].championId + " VALUES (///////////do the shit here///////////)")
+				})
 				// store this object by response.championId
 			}
 			// Do what you want with the response data
