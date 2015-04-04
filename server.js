efcf64b470f2a673ce7c7b44a0196c994d051987
Michael Liu <michael.liu95@gmail.com>
2015-04-04 14:19:34 -0500

    Fix code

--- server.js
+++ server.js
@@ -6,11 +6,10 @@ lolApi.init(config.api_key);
 
 // Takes matchId and returns a JSON object (1 depth) of data we're interested in (e.g. kills, deaths, CS, etc.)
 function storeMatchData(matchId) {
-	lolApi.getMatch(bodyJSON[i], true, 'na', function(err, response) {
+	lolApi.getMatch(matchId, true, 'na', function(err, response) {
 		if(!err) {
-			var data = JSON.parse(response);
-			// Do what you want with the data
-			console.log(data.region);
+			// Do what you want with the response data
+			console.log(response.region);
 		}
 	});
 }
