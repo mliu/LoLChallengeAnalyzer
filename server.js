8624181d85b774c3276a371a9c51b87667f0b6d3
Liam Bui <emlb598@cornell.edu/em>
2015-04-04 17:58:22 -0400

    test test

--- server.js
+++ server.js
@@ -19,10 +19,9 @@ function storeMatchData(matchId) {
 					"gold": response.participants[participant].stats.goldEarned,
 					"cs": response.participants[participant].stats.minionsKilled 
 				}
-				console.log(data);
 				db.serialize(function() {
-					db.run("CREATE TABLE if not exists " + response.participants[participant].championId + " (kills INTEGER) (assists INTEGER) (deaths INTEGER) (win BOOLEAN) (gold INTEGER) (cs INTEGER)");
-					db.run("INSERT INTO " + response.participants[participant].championId + " VALUES (///////////do the shit here///////////)")
+					db.run("CREATE TABLE if not exists " + response.participants[participant].championId + " (kills INTEGER, assists INTEGER, deaths INTEGER, win BOOLEAN, gold INTEGER, cs INTEGER)");
+					db.run("INSERT INTO " + response.participants[participant].championId + "(kills, assists, deaths, win, gold, cs) VALUES (" + response.participants[participant].stats.kills + ", " + response.participants[participant].stats.assists + ", " + response.participants[participant].stats.deaths + ", " + response.participants[participant].stats.winner + ", " + response.participants[participant].stats.goldEarned + ", " + response.participants[participant].stats.minionsKilled + ")");
 				})
 				// store this object by response.championId
 			}
