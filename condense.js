f94a05aa53c2dcfe457e45d9a3efbfa75a5662cd
Liam Bui <emlb598@cornell.edu/em>
2015-04-18 00:22:15 -0400

    combininator

--- condense.js
+++ condense.js
@@ -13,20 +13,21 @@ function combininator() {
 	source.each("SELECT * FROM champ_info", function (err, one_champion) {
 		if (!err) {
 			db.run("SELECT * FROM champ_info WHERE champion_id=$champion_id", {$champion_id: one_champion.champion_id}, function (err, total_champion) {
-				if (total_champion != undefined) {
+				if (total_champion) {
 					db.run("UPDATE champ_info SET kills=$kills, assists=$assists, deaths=$deaths, win=$win, gold=$gold, cs=$cs WHERE champion_id=$champion_id", {
 						$kills: total_champion.kills + one_champion.kills,
 						$assists: total_champion.assists + one_champion.assists,
 						$deaths: total_champion.deaths + one_champion.deaths,
-						$win: total_champion.win + one_champion.win,
+						$win: ~~total_champion.win + ~~one_champion.win,
 						$gold: total_champion.gold + one_champion.gold,
 						$cs: total_champion.cs + one_champion.cs,
 						$champion_id: one_champion.champion_id
-					}, function (err, response) { 
+					}, function (err, response) {
 						if (err) {
 							console.log(err);
 						}
 					});
+					console.log("updating");
 				}
 				else {
 					db.run("INSERT INTO champ_info(champion_id, kills, assists, deaths, win, gold, cs) VALUES ($champion_id, $kills, $assists, $deaths, $win, $gold, $cs)", {
@@ -34,7 +35,7 @@ function combininator() {
 						$kills: one_champion.kills,
 						$assists: one_champion.assists,
 						$deaths: one_champion.deaths,
-						$win: one_champion.win,
+						$win: ~~one_champion.win,
 						$gold: one_champion.gold,
 						$cs: one_champion.cs
 					}, function (err, response) {
